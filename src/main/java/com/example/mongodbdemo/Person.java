0d225f2f0b849401c4f947c66d9424354679e951
niaoshuai <niao.shuai123@163.com>
2022-06-21 16:45:24 +0800

    增加连接池配置

--- src/main/java/com/example/mongodbdemo/Person.java
+++ src/main/java/com/example/mongodbdemo/Person.java
@@ -1,6 +1,5 @@
 package com.example.mongodbdemo;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -9,7 +8,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
  *
  */
 @Data
-@AllArgsConstructor
 @Document
 public class Person {
     @Id
--- src/main/java/com/example/mongodbdemo/PersonController.java
+++ src/main/java/com/example/mongodbdemo/PersonController.java
@@ -3,6 +3,7 @@ package com.example.mongodbdemo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +20,7 @@ public class PersonController {
     private final MongoTemplate mongoTemplate;
 
     @PostMapping
-    public Person insert() {
-        Person person = new Person(UUID.randomUUID().toString(),"碉堡",10);
+    public Person insert(@RequestBody Person person) {
         return mongoTemplate.insert(person);
     }
 }
